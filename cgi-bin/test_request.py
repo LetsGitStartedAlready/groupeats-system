188638dedd068c6f761fb115306481941985c105
E L <eilamemail@gmail.com>
2019-06-30 22:39:51 -0400

    Updated the test

--- cgi-bin/test_request.py
+++ cgi-bin/test_request.py
@@ -1,18 +1,11 @@
-import requests
+#!/usr/bin/python
 
-#response = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyD1gQaUJgwdFqioPQv7qdPQ_4zVH9jJpzA')
-
-url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
-params = {
-    'location':-33.8670522,
-    'radius':500,
-    'types':'food',
-    'name':'harbour',
-    'key':'AIzaSyD1gQaUJgwdFqioPQv7qdPQ_4zVH9jJpzA'
-}
-
-response = requests.get(url=url,params=params)
-
-data = response.json()
-
-print(str(data))
+print ("Content-type:text/html\r\n\r\n")
+print ('<html>')
+print ('<head>')
+print ('<title>Hello World - First CGI Program</title>')
+print ('</head>')
+print ('<body>')
+print ('<h2>Hello World! This is my first CGI program</h2>')
+print ('</body>')
+print ('</html>')
